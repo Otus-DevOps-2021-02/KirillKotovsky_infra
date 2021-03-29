9b3bb4b1d47643afb58c7be68f9ed9cd3fee534b
KirillKotovsky <insarov@gmail.com>
2021-03-29 12:33:54 +0000

    add terraform-1

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  default     = "ru-central1-a"
+}
+variable public_key_path {
+  default = "~/.ssh/appuser.pub"
+}
+variable image_id {
+  description = "Disk image"
+}
+variable subnet_id {
+  description = "Subnet"
+}
+variable service_account_key_file {
+  description = "key .json"
+}
+variable private_key {
+  description = "private_key"
+}
