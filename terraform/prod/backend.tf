e2706b3d7bd6a0dce1535c09c1f9fb7f4bc1b325
KirillKotovsky <insarov@gmail.com>
2021-04-08 13:43:17 +0000

    terraform-2+bucket

---
+++ terraform/prod/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "kirillkotosky-bucket-01"
+    region     = "ru-central1"
+    key        = "prod/terraform.tfstate"
+    access_key = "Q9PnBATtGfrhs6NURG69"
+    secret_key = "U-B8o0TEibQcPp0x-SUUexWx0XOAs46mKgF-Y6n5"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
